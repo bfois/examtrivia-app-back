d2455421b5557c67775dc98d2dbfe0d77fcff833
BraianF <97342723+bfois@users.noreply.github.com>
2023-03-11 10:02:24 -0300

    endpoint
    obtener los temas, de una materia en especifico

--- examtrivia/src/main/java/com/api/examtrivia/Controller/MateriaController.java
+++ examtrivia/src/main/java/com/api/examtrivia/Controller/MateriaController.java
@@ -1,19 +1,19 @@
 package com.api.examtrivia.Controller;
 
+
 import com.api.examtrivia.Entity.Materia;
 
+import com.api.examtrivia.Entity.Temas;
 import com.api.examtrivia.Service.MateriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("api/v1/home")
 @CrossOrigin("http://localhost:4200/")
 public class MateriaController {
 
@@ -24,6 +24,16 @@ public class MateriaController {
     public ResponseEntity<?> listAll(){
         List<Materia> materias = materiaService.listAll();
         return ResponseEntity.ok(materias);
+    }
+    @GetMapping("/materias/{id}/temas")
+    private ResponseEntity<?> listTemasByMateriaId(@PathVariable("id") int id) {
+        Optional<Materia> materia = materiaService.ListById(id);
+        if (materia.isPresent()) {
+            List<Temas> temas = materia.get().getTemas();
+            return ResponseEntity.ok(temas);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
