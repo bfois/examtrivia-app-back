cd82b03eb1ceea7e6e8f32996a08d02a4611ac2b
BraianF <97342723+bfois@users.noreply.github.com>
2023-03-30 11:25:07 -0300

    java version 11
    test

--- src/main/java/com/api/examtrivia/ExamtriviaApplication.java
+++ src/main/java/com/api/examtrivia/ExamtriviaApplication.java
@@ -1,6 +1,6 @@
 package com.api.examtrivia;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
